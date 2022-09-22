1c6f7b86ca54d92475d0a40176420861992b922d
Philipp Toepfer <p.toepfer@fm-maschinenbau.de>
2022-09-22 10:47:16 +0200

    Add initial user controller

---
+++ src/controller/UserController.ts
@@ -0,0 +1,17 @@
+import { IUser } from "domain/IUser";
+import { Express } from "express";
+
+const UserController = (app: Express) => {
+  app.get("/v1/users/:id", (req, res) => {
+    const id = req.params.id;
+    const user: IUser = {
+      email: "test@test.com",
+      firstName: "My",
+      id,
+      lastName: "Name",
+    };
+    res.json(user);
+  });
+};
+
+export default UserController;
